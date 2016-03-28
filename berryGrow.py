4b8c598343f27ca29478d6c6e8a6361fc96574ae
pablinhob <pablinho.blanco@gmail.com>
2016-03-28 12:38:53 +0200

    Lectura JSON

--- berryGrow.py
+++ berryGrow.py
@@ -1,28 +1,49 @@
 #!/usr/bin/python
 
 import RPi.GPIO as GPIO
+import os.path, time
 import pprint
+import json
 #from conf.machinesConf import *
 #from conf.machinesStatus import *
 from lib.Machine import *
 
-
-
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
+pathMachinesConf = os.getcwd()+'conf/machinesConf.json')
+pathMachinesStatus = os.getcwd()+'conf/machinesStatus.json')
+
 machines = []
-#pprint.pprint(machinesStatus)
+
+def updateMachines():
+    machinesConf = json.loads( open( pathMachinesConf ).read() )
+    machinesStatus = json.loads( open( pathMachinesStatus ).read() )
+
+    for (i, mConf) in enumerate( machinesConf ):
+        if( mConf['enabled'] == True ):
+            machines.append( Machine( mConf, machinesStatus[i] ) )
 
 
 
-schedule.clear()
-for (i, mConf) in enumerate( machinesConf ):
-    if( mConf['enabled'] == True ):
-        machines.append( Machine( mConf, machinesStatus[i] ) )
-
-
+confLastUpdate = False
+statusLastUpdate = False
 
 while True:
+
+    dateUpdateConf = time.ctime( os.path.getmtime( pathMachinesConf ) )
+    dateUpdateStatus = time.ctime( os.path.getmtime( pathMachinesStatus ) )
+
+    if(
+        dateUpdateConf > confLastUpdate ||
+        dateUpdateStatus > statusLastUpdate ||
+        confLastUpdate == False
+    ):
+        confLastUpdate = dateUpdateConf
+        statusLastUpdate = dateUpdateStatus
+        schedule.clear()
+        updateMachines()
+
+
     schedule.run_pending()
     time.sleep(1)
